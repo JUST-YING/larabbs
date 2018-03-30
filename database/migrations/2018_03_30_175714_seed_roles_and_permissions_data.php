1e1391fc17cfae8c230e6bbf8f42310631fdfa3b
JUST-YING <805623088@qq.com>
2018-03-30 13:19:32 +0000

    tijiao

---
+++ database/migrations/2018_03_30_175714_seed_roles_and_permissions_data.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class SeedRolesAndPermissionsData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
