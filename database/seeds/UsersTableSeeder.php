5e702d53c093c0c47c942370f7814006fb863b7a
JUST-YING <805623088@qq.com>
2018-04-08 09:02:07 +0000

    Integrate laravel-permission package

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -47,5 +47,12 @@ class UsersTableSeeder extends Seeder
         $user->name = 'chen';
         $user->email = '1119329472@qq.com';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
